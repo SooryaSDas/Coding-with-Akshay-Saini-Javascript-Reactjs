3166a5a0851fe052114b1e9134aa4259d3361baa
SOORYA S DAS <83228601+SooryaSDas@users.noreply.github.com>
2023-04-01 19:15:02 +0530

    Function hositing in javascript

--- JavaScript/hositingfunciton.js
+++ JavaScript/hositingfunciton.js
@@ -1,5 +1,5 @@
 // Hoisting in Javascript Funcitons
-var num = 10;
+var x = 10;
 // Function invocations
 a();
 b();
@@ -11,12 +11,16 @@ function a(){
 }
 // function b()
 function b(){
-    var y = 5;
-    console.log(y);
+    var x = 5;
+    console.log(x);
 }
 
 // In this code exection, global execution context was created.
-// created the memory and code component. firstly created the varibale num and assign the value undefined
+// created the memory and code component. firstly created the varibale x and assign the value undefined
 // then move to funciton a(),b() , it take full function code.
-// creat the code component. assign the value 10 to the variable num.
+// creat the code component. assign the value 10 to the variable x.
 // move to line number 4, function a() invocation, it create the execution context, if the exection cotext created it will go to call stack.
+// memory assign x is undefined, after the code component 10 is assigned to x variable.
+// Move to funciton a, variable x is assign to 4, in console, searching for varibale x locally and print 4 in console.
+// move the function b, assign the value 5 to variable x, console the value x, searching locally and find it is in 5. print it.
+// after the full exection completed the execution context was deleted. even the call stack global exection context also deleted.
