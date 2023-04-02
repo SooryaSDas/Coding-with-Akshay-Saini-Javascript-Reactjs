a0004fa1727a15909afe91517597a53dd38c7687
SOORYA S DAS <83228601+SooryaSDas@users.noreply.github.com>
2023-04-02 14:31:19 +0530

    Undefined and Not defined in javascript

--- JavaScript/undefined vs not defined.js
+++ JavaScript/undefined vs not defined.js
@@ -5,3 +5,9 @@ console.log(c); // not defined
 var a = 5;
 console.log(a); // 5
 
+// Undefined 
+    // Memory allocated but not define a value
+    // Undefined is not equal to empty
+
+// Not defined
+    // the variable is not declared
