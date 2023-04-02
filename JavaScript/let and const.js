8a202efd6fa3e9cc0a8163c20c380e14cbd4e336
SOORYA S DAS <83228601+SooryaSDas@users.noreply.github.com>
2023-04-02 14:34:52 +0530

    Let and Const in javascript

---
+++ JavaScript/let and const.js
@@ -0,0 +1,10 @@
+// let & const
+
+let a = 10;
+console.log(a); // output : 10
+let a = 20; // display the error identifier a has already been declared.
+const c = 50;
+console.log(c); // output will be : 50
+const b;
+b = 40; // display the error missing initializer in const declaration.
+
