36ecdd0baf030d3d9dfa0e1e02a1519186df6ae2
SOORYA S DAS <83228601+SooryaSDas@users.noreply.github.com>
2023-04-02 14:33:44 +0530

    Scope chain in javascript

--- JavaScript/scope chain.js
+++ JavaScript/scope chain.js
@@ -13,3 +13,11 @@ function a(){
 a();
 
 // output 10;
+// function c is lexically inside of function b, and the function b is lexically inside the funciton a()
+// inside of function a the variable num was created
+// inside the function c, consoling num variable, function will search num variable in locally 
+// it's not find then move to his lexical parent b
+// not find move to the lexical parent a, find the num variable and print in the console.
+
+
+// lexical environment is the local memory + reference to the lexical environment of it's parentent.
