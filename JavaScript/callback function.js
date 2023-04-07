e17162e2beaa51c12824abf6dba4bd6d65c13faf
SOORYA S DAS <83228601+SooryaSDas@users.noreply.github.com>
2023-04-07 15:16:28 +0530

    callback function in js

---
+++ JavaScript/callback function.js
@@ -0,0 +1,15 @@
+setTimeout(function(){
+    console.log("timer");
+},3000);
+function x(y){
+    console.log("x");
+    y();
+}
+x(function y(){
+    console.log("one");
+});
+
+// output
+// x
+// one
+// timer
