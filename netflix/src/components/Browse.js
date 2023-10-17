ca2ce46b9560374961913c1fcc0652d1fcdfb1ad
SooryaSDas <sooryasandra6@gmail.com>
2023-10-17 22:20:23 +0530

    main page trailer and movie data added

--- netflix/src/components/Browse.js
+++ netflix/src/components/Browse.js
@@ -1,19 +1,15 @@
-import { useEffect } from "react";
-import { API_OPTIONS } from "../utils/constants"
-import Header from "./Header"
-const Browse = ()=>{
-    const getNowPlayingMovies = async ()=>{
-        const data = await fetch('https://api.themoviedb.org/3/movie/now_playing?page=1', API_OPTIONS);
-        const json = await data.json();
-        console.log(json);
-    }
+import Header from "./Header";
+import useNowPlayingMovies from "./Hooks/useNowPlayingMovies"; 
+import MainContainer from "./MainContainer";
+import SecondaryContainer from "./SecondaryContainer";
 
-    useEffect(()=>{
-        getNowPlayingMovies();
-    },[])
+const Browse = ()=>{
+    useNowPlayingMovies();
     return(
         <div>
             <Header/>
+            <MainContainer/>
+            <SecondaryContainer/>
         </div>
     )
 }
---
+++ netflix/src/components/SecondaryContainer.js
@@ -0,0 +1,9 @@
+const SecondaryContainer = ()=>{
+    return (
+        <div>
+            <h2>secondary container</h2>
+        </div>
+    )
+}
+
+export default SecondaryContainer;
